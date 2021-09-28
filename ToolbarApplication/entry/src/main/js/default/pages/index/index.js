79718b280488027599046b5195edd35fe7fbe51e
煮酒 <1095610623@qq.com>
2021-09-28 06:52:14 +0000

    update ToolbarApplication/entry/src/main/js/default/pages/index/index.js.

--- ToolbarApplication/entry/src/main/js/default/pages/index/index.js
+++ ToolbarApplication/entry/src/main/js/default/pages/index/index.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 export default {
     data: {
         fontWeight0: 'bolder',
@@ -10,13 +24,6 @@ export default {
         display3: 'none',
         display4: 'none',
         display5: 'none',
-<<<<<<< HEAD
-<<<<<<< HEAD
-        display6: 'none',
-=======
->>>>>>> c56036265ce4f59f70e0aa5f2bc7ee97143c35e6
-=======
->>>>>>> 4dde2e0782ffdf5a089ef773845d0fdc72cd42f8
         imgGr: 'common/Icon/1.png',
         imgDu: 'common/Icon/2.png',
         imgGu: 'common/Icon/3.png',
@@ -47,13 +54,6 @@ export default {
         this.display3 = 'none';
         this.display4 = 'none';
         this.display5 = 'none';
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.display6 = 'none';
-=======
->>>>>>> c56036265ce4f59f70e0aa5f2bc7ee97143c35e6
-=======
->>>>>>> 4dde2e0782ffdf5a089ef773845d0fdc72cd42f8
     },
     function2() {
         this.fontWeight0 = 'normal';
@@ -66,14 +66,6 @@ export default {
         this.display3 = 'none';
         this.display4 = 'none';
         this.display5 = 'none';
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.display6 = 'none';
-
-=======
->>>>>>> c56036265ce4f59f70e0aa5f2bc7ee97143c35e6
-=======
->>>>>>> 4dde2e0782ffdf5a089ef773845d0fdc72cd42f8
     },
     function3() {
         this.fontWeight0 = 'normal';
@@ -86,14 +78,6 @@ export default {
         this.display3 = 'flex';
         this.display4 = 'none';
         this.display5 = 'none';
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.display6 = 'none';
-
-=======
->>>>>>> c56036265ce4f59f70e0aa5f2bc7ee97143c35e6
-=======
->>>>>>> 4dde2e0782ffdf5a089ef773845d0fdc72cd42f8
     },
     function4() {
         this.fontWeight0 = 'normal';
@@ -106,14 +90,6 @@ export default {
         this.display3 = 'none';
         this.display4 = 'flex';
         this.display5 = 'none';
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.display6 = 'none';
-
-=======
->>>>>>> c56036265ce4f59f70e0aa5f2bc7ee97143c35e6
-=======
->>>>>>> 4dde2e0782ffdf5a089ef773845d0fdc72cd42f8
     },
     function5() {
         this.fontWeight0 = 'normal';
@@ -126,12 +102,5 @@ export default {
         this.display3 = 'none';
         this.display4 = 'none';
         this.display5 = 'flex';
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.display6 = 'none';
-=======
->>>>>>> c56036265ce4f59f70e0aa5f2bc7ee97143c35e6
-=======
->>>>>>> 4dde2e0782ffdf5a089ef773845d0fdc72cd42f8
     }
 };
