bff26a3c8576ff97a630e049637d8b39e9845113
陈伟权 <9145848+wiggin_chen@user.noreply.gitee.com>
2021-11-15 05:00:26 +0000

    !122 update * update

--- HarmonyOSNewsClient/entry/src/main/java/com/huawei/codelab/provider/DevicesListProvider.java
+++ HarmonyOSNewsClient/entry/src/main/java/com/huawei/codelab/provider/DevicesListProvider.java
@@ -13,78 +13,78 @@
  * limitations under the License.
  */
 
-package com.huawei.codelab.provider;
+// package com.huawei.codelab.provider;
 
-import com.huawei.codelab.ResourceTable;
+// import com.huawei.codelab.ResourceTable;
 
-import ohos.agp.components.BaseItemProvider;
-import ohos.agp.components.Component;
-import ohos.agp.components.ComponentContainer;
-import ohos.agp.components.LayoutScatter;
-import ohos.agp.components.Text;
-import ohos.app.Context;
-import ohos.distributedschedule.interwork.DeviceInfo;
+// import ohos.agp.components.BaseItemProvider;
+// import ohos.agp.components.Component;
+// import ohos.agp.components.ComponentContainer;
+// import ohos.agp.components.LayoutScatter;
+// import ohos.agp.components.Text;
+// import ohos.app.Context;
+// import ohos.distributedschedule.interwork.DeviceInfo;
 
-import java.util.List;
+// import java.util.List;
 
-/**
- * Device list adapter
- *
- * @since 2020-12-04
- */
-public class DevicesListProvider extends BaseItemProvider {
-    private List<DeviceInfo> deviceInfoList;
-    private Context context;
+// /**
+//  * Device list adapter
+//  *
+//  * @since 2020-12-04
+//  */
+// public class DevicesListProvider extends BaseItemProvider {
+//     private List<DeviceInfo> deviceInfoList;
+//     private Context context;
 
-    /**
-     * constructor function
-     *
-     * @param listBasicInfo list info
-     * @param context context
-     * @since 2020-12-04
-     */
-    public DevicesListProvider(List<DeviceInfo> listBasicInfo, Context context) {
-        this.deviceInfoList = listBasicInfo;
-        this.context = context;
-    }
+//     /**
+//      * constructor function
+//      *
+//      * @param listBasicInfo list info
+//      * @param context context
+//      * @since 2020-12-04
+//      */
+//     public DevicesListProvider(List<DeviceInfo> listBasicInfo, Context context) {
+//         this.deviceInfoList = listBasicInfo;
+//         this.context = context;
+//     }
 
-    @Override
-    public int getCount() {
-        return deviceInfoList == null ? 0 : deviceInfoList.size();
-    }
+//     @Override
+//     public int getCount() {
+//         return deviceInfoList == null ? 0 : deviceInfoList.size();
+//     }
 
-    @Override
-    public Object getItem(int position) {
-        return deviceInfoList.get(position);
-    }
+//     @Override
+//     public Object getItem(int position) {
+//         return deviceInfoList.get(position);
+//     }
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+//     @Override
+//     public long getItemId(int position) {
+//         return position;
+//     }
 
-    @Override
-    public Component getComponent(int position, Component component, ComponentContainer componentContainer) {
-        ViewHolder viewHolder;
-        Component temp = component;
-        if (temp == null) {
-            temp = LayoutScatter.getInstance(context).parse(ResourceTable.Layout_device_list_item, null, false);
-            viewHolder = new ViewHolder();
-            viewHolder.devicesName = (Text) temp.findComponentById(ResourceTable.Id_item_child_textview);
-            temp.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) temp.getTag();
-        }
-        viewHolder.devicesName.setText(deviceInfoList.get(position).getDeviceName());
-        return temp;
-    }
+//     @Override
+//     public Component getComponent(int position, Component component, ComponentContainer componentContainer) {
+//         ViewHolder viewHolder;
+//         Component temp = component;
+//         if (temp == null) {
+//             temp = LayoutScatter.getInstance(context).parse(ResourceTable.Layout_device_list_item, null, false);
+//             viewHolder = new ViewHolder();
+//             viewHolder.devicesName = (Text) temp.findComponentById(ResourceTable.Id_item_child_textview);
+//             temp.setTag(viewHolder);
+//         } else {
+//             viewHolder = (ViewHolder) temp.getTag();
+//         }
+//         viewHolder.devicesName.setText(deviceInfoList.get(position).getDeviceName());
+//         return temp;
+//     }
 
-    /**
-     * ViewHolder which has devicesName
-     *
-     * @since 2020-12-04
-     */
-    private static class ViewHolder {
-        private Text devicesName;
-    }
-}
+//     /**
+//      * ViewHolder which has devicesName
+//      *
+//      * @since 2020-12-04
+//      */
+//     private static class ViewHolder {
+//         private Text devicesName;
+//     }
+// }
