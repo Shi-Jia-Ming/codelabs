15591f47acb4f3de1df2cae6a1956bfcd3834a24
wf0304-gme <452399386@qq.com>
2023-04-21 08:10:30 +0800

    20230421新Codelabs上架-图片编辑（ArkTS）

---
+++ ImageEdit/entry/src/main/ets/viewModel/OptionViewModel.ts
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License,Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Crop type.
+ */
+export enum CropType {
+  ORIGINAL_IMAGE,
+  ONE2ONE,
+  FOUR2THREE,
+  SIXTEEN2NINE
+}
+
+/**
+ * Rotate type
+ */
+export enum RotateType {
+  CLOCKWISE,
+  ANTI_CLOCK
+}
+
+/**
+ * Adjust type.
+ */
+export enum AdjustId {
+  BRIGHTNESS,
+  TRANSPARENCY,
+  SATURATION
+}
+
+/**
+ * Main page tab type.
+ */
+export enum MainTabId {
+  CROP,
+  ROTATE,
+  ADJUST
+}
+
+/**
+ * RGB color, red,green and blue.
+ */
+export enum RGBIndex {
+  RED,
+  GREEN,
+  BLUE
+}
+
+/**
+ * HSV type.
+ */
+export enum HSVIndex {
+  HUE,
+  SATURATION,
+  VALUE
+}
+
+/**
+ * Angel range.
+ */
+export enum AngelRange {
+  ANGEL_0_60,
+  ANGEL_60_120,
+  ANGEL_120_180,
+  ANGEL_180_240,
+  ANGEL_240_300,
+  ANGEL_300_360
+}
---
+++ ImageEdit/entry/src/main/ets/workers/AdjustSaturationWork.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License,Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import hilog from '@ohos.hilog';
+import worker, { ThreadWorkerGlobalScope, MessageEvents, ErrorEvent } from '@ohos.worker';
+import { adjustSaturation } from '../utils/AdjustUtil';
+
+let workerPort : ThreadWorkerGlobalScope = worker.workerPort;
+
+/**
+* Defines the event handler to be called when the worker thread receives a message sent by the host thread.
+* The event handler is executed in the worker thread.
+*
+* @param e message data
+*/
+workerPort.onmessage = function(event : MessageEvents) {
+    let bufferArray = event.data.buf;
+    let last = event.data.last;
+    let cur = event.data.cur;
+    let buffer = adjustSaturation(bufferArray, last, cur)
+    workerPort.postMessage(buffer);
+}
+
+/**
+* Defines the event handler to be called when the worker receives a message that cannot be deserialized.
+* The event handler is executed in the worker thread.
+*
+* @param e message data
+*/
+workerPort.onmessageerror = function(event : MessageEvents) {
+    hilog.error(0x0000, 'AdjustSaturationWork', 'Failed to load the content. Cause: %{public}s', `on message error ${JSON.stringify(event)}`);
+}
+
+/**
+* Defines the event handler to be called when an exception occurs during worker execution.
+* The event handler is executed in the worker thread.
+*
+* @param e error message
+*/
+workerPort.onerror = function(error : ErrorEvent) {
+    hilog.error(0x0000, 'AdjustSaturationWork', 'Failed to load the content. Cause: %{public}s', `on worker error ${JSON.stringify(error)}`);
+}
