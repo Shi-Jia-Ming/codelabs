da3d4e6677e8653db5536a8a1c52ea09aecb2013
wf0304-gme <452399386@qq.com>
2023-04-21 08:38:38 +0800

    20230421新Codelabs上架-UIAbility和自定义组件生命周期（ArkTS）

---
+++ UIAbilityLifeCycle/README.md
@@ -0,0 +1,21 @@
+# UIAbility和自定义组件生命周期（ArkTS）
+
+### 简介
+本篇Codelab主要介绍UIAbility和自定义组件生命周期。
+
+### 相关概念
+- [UIAbility](https://developer.harmonyos.com/cn/docs/documentation/doc-guides-V3/uiability-overview-0000001477980929-V3)：UIAbility组件是一种包含UI界面的应用组件，主要用于和用户交互。UIAbility组件是系统调度的基本单元，为应用提供绘制界面的窗口；一个UIAbility组件中可以通过多个页面来实现一个功能模块。每一个UIAbility组件实例，都对应于一个最近任务列表中的任务。
+- [自定义组件](https://developer.harmonyos.com/cn/docs/documentation/doc-guides-V3/ui-js-building-ui-component-0000001478061525-V3)：自定义组件是由开发者定义的具有一定特性的组件，通过注解@Component实现，可以精确控制屏幕元素的外观，也可响应用户的点击、触摸、长按等操作。
+
+### 相关权限
+不涉及
+
+### 使用说明
+不涉及
+
+### 约束与限制
+1. 本示例仅支持标准系统上运行，支持设备：华为手机或运行在DevEco Studio上的华为手机设备模拟器。
+2. 本示例为Stage模型，支持API version 9及以上版本SDK。
+3. 本示例需要使用DevEco Studio 3.1 Beta2及以上版本进行编译运行。
+
+
---
+++ UIAbilityLifeCycle/entry/src/main/ets/entryability/EntryAbility.ts
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIAbility from '@ohos.app.ability.UIAbility';
+import hilog from '@ohos.hilog';
+import window from '@ohos.window';
+import Want from '@ohos.app.ability.Want';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+
+export default class EntryAbility extends UIAbility {
+    windowStage: window.WindowStage;
+    tag: string = 'EntryAbility';
+    domain: number = 0x0000;
+    want: Want;
+    launchParam: AbilityConstant.LaunchParam;
+
+    onCreate(want, launchParam) {
+        this.want = want;
+        this.launchParam = launchParam;
+        hilog.info(this.domain, this.tag, 'Ability is onCreate.');
+    }
+
+    onDestroy() {
+        hilog.info(this.domain, this.tag, 'Ability is onDestroy.');
+    }
+
+    onWindowStageCreate(windowStage: window.WindowStage) {
+        this.windowStage = windowStage;
+
+        // Setting Event Subscription for WindowStage (Obtained/Out-of-focus, Visible/Invisible)
+        try {
+            windowStage.on('windowStageEvent', (data) => {
+                hilog.info(
+                this.domain,
+                    'Succeeded in enabling the listener for window stage event changes. Data: %{public}',
+                    JSON.stringify(data)?? ''
+                );
+            });
+        } catch (exception) {
+            hilog.error(
+            this.domain,
+                'Failed to enable the listener for window stage event changes. Cause: %{public}',
+                JSON.stringify(exception)?? ''
+            );
+        }
+
+        // Main window is created, set main page for this ability
+        // Setting UI Loading
+        windowStage.loadContent('pages/LifeCyclePage', (err, data) => {
+            if (err.code) {
+                hilog.error(this.domain, 'testTag', 'Failed to load the content. Cause: %{public}s',
+                    JSON.stringify(err) ?? '');
+                return;
+            }
+            hilog.info(this.domain, this.tag, 'Succeeded in loading the content. Data: %{public}s',
+                JSON.stringify(data) ?? '');
+        });
+    }
+
+    onWindowStageDestroy() {
+        // Releasing UI Resources
+        // Unregisters the WindowStage event for getting/out of focus in onWindowStageDestroy()
+        try {
+            this.windowStage.off('windowStageEvent');
+        } catch (exception) {
+            hilog.error(this.domain, 'Failed to disable the listener for window stage event changes. Cause: %{public}s',
+            JSON.stringify(exception));
+        }
+    }
+
+    onForeground() {
+        // Ability has brought to foreground
+        hilog.info(this.domain, this.tag, 'Ability is onForeground.');
+    }
+
+    onBackground() {
+        // Ability has back to background
+        hilog.info(this.domain, this.tag, 'Ability is onBackground.');
+    }
+}
