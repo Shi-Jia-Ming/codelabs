c7d4a1d0d55327e251dd18156db9e2f82e0ea6ca
13871184879 <452399386@qq.com>
2023-10-07 11:40:34 +0800

    更新升级关系型数据库（ArkTS）

--- Rdb/entry/src/main/ets/entryability/EntryAbility.ts
+++ Rdb/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License,Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,8 +14,8 @@
  */
 
 import hilog from '@ohos.hilog';
-import UIAbility from '@ohos.app.ability.UIAbility'
-import Window from '@ohos.window'
+import UIAbility from '@ohos.app.ability.UIAbility';
+import Window from '@ohos.window';
 
 export default class entryAbility extends UIAbility {
     onCreate(want, launchParam) {
